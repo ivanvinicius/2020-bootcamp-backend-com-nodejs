c117a96dde391aeb941759c2c62284f6df9c48d3
ivanvinicius <ivanvinicius88@gmail.com>
2020-05-09 15:30:35 -0300

    metodos http

--- src/index.js
+++ src/index.js
@@ -4,10 +4,35 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'hello' });
+app.get('/projects', (request, response) => {
+  return response.json([
+    'projeto 1',
+    'projeto 2'
+  ]);
+});
+
+app.post('/projects', (request, response) => {
+  return response.json([
+    'projeto 1',
+    'projeto 2',
+    'projeto 3'
+  ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+  return response.json([
+    'projeto 4',
+    'projeto 2',
+    'projeto 3'
+  ]);
+});
+
+app.delete('/projects', (request, response) => {
+  return response.json([
+    'projeto 2'
+  ]);
 });
 
 app.listen(3333, () => {
-  console.log('🚀 Backend is running!');
+  console.log('🚀 backend is running!');
 });
