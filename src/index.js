f697939a0eee0d31fc6bcb769605865574003185
ivanvinicius <ivanvinicius88@gmail.com>
2020-05-09 15:54:55 -0300

    tipos de parâmetros na url

--- src/index.js
+++ src/index.js
@@ -4,35 +4,24 @@ const app = express();
 
 app.use(express.json());
 
+/**
+ * Tipos de Parâmetros
+ * 
+ * Query params : Filtros e Paginação (projects?title=reactjs&author=ivan)
+ * Route params : Identificar recursos em PUT e DELETE (projects/1)
+ * Request Body : Conteúdo no POST e PUT ({ "name": "Ivan Vinicius Boneti" })
+ */
+
 app.get('/projects', (request, response) => {
-  return response.json([
-    'projeto 1',
-    'projeto 2'
-  ]);
+  const { title, author } = request.query;
+  const { id } = request.params;
+  const { name } = request.body;
 });
 
-app.post('/projects', (request, response) => {
-  return response.json([
-    'projeto 1',
-    'projeto 2',
-    'projeto 3'
-  ]);
-});
+app.post('/projects', (request, response) => {});
 
-app.put('/projects/:id', (request, response) => {
-  return response.json([
-    'projeto 4',
-    'projeto 2',
-    'projeto 3'
-  ]);
-});
+app.put('/projects/:id', (request, response) => {});
 
-app.delete('/projects', (request, response) => {
-  return response.json([
-    'projeto 2'
-  ]);
-});
+app.delete('/projects/:id', (request, response) => {});
 
-app.listen(3333, () => {
-  console.log('🚀 backend is running!');
-});
+app.listen(3333, () => console.log('🚀 backend is running!'));
