8ab75e1db93f985c2b91368699ea583009e4dc8e
ivanvinicius <ivanvinicius88@gmail.com>
2020-05-10 18:48:00 -0300

    middlewares

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const app = express();
 
@@ -13,7 +13,38 @@ app.use(express.json());
  * Request Body : Conteúdo no POST e PUT                ({ "name": "Ivan Vinicius Boneti" })
  */
 
+ /**
+  * Middlewares
+  * 
+  * Interceptador de requisições que pode interromper uma requisição ou alterar dados da mesma
+  */
+
 const projects = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.time(logLabel)
+  
+  next();
+
+  console.timeEnd(logLabel)
+}
+
+function validadeProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id)) {
+    return response.status(400).json({ error: 'Invalid project ID.' });
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validadeProjectId);
 
 app.get('/projects', (request, response) => {
   const { title } = request.query;
